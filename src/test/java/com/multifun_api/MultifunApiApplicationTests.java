b51ba2799202c460690f43055b70c0d3a8d1e045
Ivan Lozoya <lozoya.oscar.ivan@gmail.com>
2024-07-09 14:46:40 -0600

    Initial commit

---
+++ src/test/java/com/multifun_api/MultifunApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multifun_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultifunApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
