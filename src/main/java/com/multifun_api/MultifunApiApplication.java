ae52470343d71c5a2a9010678c99bbe14378e27a
Ivan Lozoya <lozoya.oscar.ivan@gmail.com>
2024-07-09 15:40:00 -0600

    first commit in correct branch

---
+++ src/main/java/com/multifun_api/MultifunApiApplication.java
@@ -0,0 +1,13 @@
+package com.multifun_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultifunApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultifunApiApplication.class, args);
+	}
+
+}
